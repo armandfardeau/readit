d5b057ea99bbb686ef5340efedae764e79c2f65d
Armand Fardeau <fardeauarmand@gmail.com>
2017-03-05 23:29:57 +0100

    add test and correct error on view when there is no story yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304190640) do
+ActiveRecord::Schema.define(version: 20170304150305) do
 
   create_table "stories", force: :cascade do |t|
     t.string   "name"
     t.string   "link"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "age"
-    t.string   "mail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
