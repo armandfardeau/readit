0974e2fcecc506a5474fa0c248adef9ea1da4bea
Armand Fardeau <fardeauarmand@gmail.com>
2017-03-05 15:03:16 +0100

    add form and css

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -6,4 +6,15 @@ class StoriesController < ApplicationController
   def new
     @story = Story.new
   end
+
+  def create
+    @story = Story.new(story_params)
+    @story.save
+    redirect_to stories_path
+  end
+
+private
+  def story_params
+    params.require(:story).permit(:name, :link)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'stories/index'
-  get 'stories/new'
+  resources :stories
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
