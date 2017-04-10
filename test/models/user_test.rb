a44877fc629f5aacc3bbb6cf62c7f2d992e47721
Armand Fardeau <fardeauarmand@gmail.com>
2017-04-10 12:56:35 +0200

    add test on user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,5 +4,9 @@ class UserTest < ActiveSupport::TestCase
   test "has a story association" do
     assert_equal 2, users(:glenn).stories.size
     assert users(:glenn).stories.includes stories(:one)
+    end
+  test "has a votes association" do
+    assert_equal 1, users(:glenn).votes.size
+    assert users(:john).votes.includes votes(:two)
   end
 end
