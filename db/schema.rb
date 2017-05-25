4b87143061a9ef3cc0c625499f0e6e76f01f99d4
Armand Fardeau <fardeauarmand@gmail.com>
2017-05-25 16:36:09 +0200

    add counter cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325233637) do
+ActiveRecord::Schema.define(version: 20170525114034) do
 
   create_table "stories", force: :cascade do |t|
     t.string   "name"
     t.string   "link"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.integer  "user_id"
+    t.integer  "votes_count", default: 0
   end
 
   create_table "users", force: :cascade do |t|
