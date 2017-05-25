eac584f1a1380d4f56508be8d6c44fdc56c37ec7
Armand Fardeau <fardeauarmand@gmail.com>
2017-05-25 16:44:23 +0200

    uodate controller index with new request

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,7 @@
 class StoriesController < ApplicationController
   before_action :ensure_login, only: [:new, :create]
   def index
-    @story = Story.order('RANDOM()').first
+    @story = Story.where('vote_count >=5').order('id DESC')
     @count = Story.count
   end
 
