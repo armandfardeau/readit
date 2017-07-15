9886d0ba76397bff8af0d485afffe4aee1baf56e
Armand Fardeau <fardeauarmand@gmail.com>
2017-07-15 18:03:30 +0200

    had a trough relation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :stories
   has_many :votes
+  has_many :stories_voted_on,
+           trough: :votes,
+           source: :story
 end
+
