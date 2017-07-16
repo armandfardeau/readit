72723b6614dcd88d080f4dd9b0089b0f31112061
Armand Fardeau <fardeauarmand@gmail.com>
2017-07-16 17:50:18 +0200

    add method to controller users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @stories_sumbitted = @user.stories.limit(6).order('stories.id DESC')
+    @stories_voted_on = @user.stories_voted_on.limit(6).order('votes.id DESC')
   end
 end
