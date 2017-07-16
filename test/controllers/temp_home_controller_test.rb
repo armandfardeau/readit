8c912ec780581698398d5df16b17d5dd30d7babe
Armand Fardeau <fardeauarmand@gmail.com>
2017-07-16 18:02:55 +0200

    del useless unit test for temp controller

--- test/controllers/temp_home_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TempHomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get redirect" do
-    get stories_path
-    assert_response :success
-  end
-
-end
