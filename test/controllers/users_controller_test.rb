a76ebce4475c0140b7a671246a10d00159e51a6f
Armand Fardeau <fardeauarmand@gmail.com>
2017-07-16 18:55:23 +0200

    remove auto generated test for users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get users_show_url
-    assert_response :success
-  end
 
   test "show user" do
     get user_path(users(:glenn))
