82b7e315c5fd675abf4008869c95573b41b7bb26
Armand Fardeau <fardeauarmand@gmail.com>
2017-07-17 20:49:19 +0200

    create controller Tags show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'stories#index'
 
   resources :stories do
@@ -9,5 +10,6 @@ Rails.application.routes.draw do
   end
   resource :session
   resources :users
+  resources :tags
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
